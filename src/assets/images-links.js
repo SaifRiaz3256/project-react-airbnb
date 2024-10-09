113b3d8cc50cbae56e99d692f36ac090ede2e7cb
Saif3256 <167188461+Saif3256@users.noreply.github.com>
2024-10-09 15:08:02 +0500

    Add images to cards

--- src/assets/images-links.js
+++ src/assets/images-links.js
@@ -1,117 +1,143 @@
 export const links = [
   {
-    label: "Amazing",
+    label: "Icons",
+    imgSrc:
+      "https://a0.muscache.com/im/pictures/mediaverse/category_icon/original/3e5243c8-4d15-4c6b-97e3-7ba2bb7bb880.png",
+  },
+  {
+    label: "Top Cities",
+    imgSrc:
+      "https://a0.muscache.com/pictures/ed8b9e47-609b-44c2-9768-33e6a22eccb2.jpg",
+  },
+  {
+    label: "Rooms",
+    imgSrc:
+      "https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg",
+  },
+  {
+    label: "Caves",
+    imgSrc:
+      "https://a0.muscache.com/pictures/4221e293-4770-4ea8-a4fa-9972158d4004.jpg",
+  },
+  {
+    label: "Amazing views",
     imgSrc:
       "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
   },
   {
-    label: "Top of the world",
+    label: "Windmill",
     imgSrc:
-      "https://a0.muscache.com/pictures/248f85bf-e35e-4dc3-a9a1-e1dbff9a3db4.jpg",
+      "https://a0.muscache.com/pictures/5cdb8451-8f75-4c5f-a17d-33ee228e3db8.jpg",
   },
   {
-    label: "Farms",
+    label: "Mansions",
     imgSrc:
-      "https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg",
+      "https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg",
   },
   {
-    label: "Surfing",
-    imgSrc:
-      "https://a0.muscache.com/pictures/957f8022-dfd7-426c-99fd-77ed792f6d7a.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/677a041d-7264-4c45-bb72-52bff21eb6e8.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/c0fa9598-4e37-40f3-b734-4bd0e2377add.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
-  },
-  {
-    label: "Lakefront",
+    label: "Tropical",
     imgSrc:
       "https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg",
   },
   {
-    label: "Lakefront",
+    label: "Tiny Homes",
     imgSrc:
-      "https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg",
+      "https://a0.muscache.com/pictures/3271df99-f071-4ecf-9128-eb2d2b1f50f0.jpg",
   },
   {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/c0fa9598-4e37-40f3-b734-4bd0e2377add.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
-  },
-  {
-    label: "Lakefront",
-    imgSrc:
-      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
-  },
-  {
-    label: "Lakefront",
+    label: "OMG",
     imgSrc:
       "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
   },
   {
-    label: "Lakefront",
+    label: "Trending",
     imgSrc:
-      "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
+      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
   },
   {
-    label: "Lakefront",
+    label: "Earth Homes",
     imgSrc:
-      "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
+      "https://a0.muscache.com/pictures/d7445031-62c4-46d0-91c3-4f29f9790f7a.jpg",
   },
   {
-    label: "Lakefront",
+    label: "Castle",
+    imgSrc:
+      "https://a0.muscache.com/pictures/1b6a8b70-a3b6-48b5-88e1-2243d9172c06.jpg",
+  },
+  {
+    label: "Bed and BreakFast",
+    imgSrc:
+      "https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg",
+  },
+  {
+    label: "Icons",
+    imgSrc:
+      "https://a0.muscache.com/im/pictures/mediaverse/category_icon/original/3e5243c8-4d15-4c6b-97e3-7ba2bb7bb880.png",
+  },
+  {
+    label: "Top Cities",
+    imgSrc:
+      "https://a0.muscache.com/pictures/ed8b9e47-609b-44c2-9768-33e6a22eccb2.jpg",
+  },
+  {
+    label: "Rooms",
+    imgSrc:
+      "https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg",
+  },
+  {
+    label: "Caves",
+    imgSrc:
+      "https://a0.muscache.com/pictures/4221e293-4770-4ea8-a4fa-9972158d4004.jpg",
+  },
+  {
+    label: "Amazing views",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+  },
+  {
+    label: "Windmill",
+    imgSrc:
+      "https://a0.muscache.com/pictures/5cdb8451-8f75-4c5f-a17d-33ee228e3db8.jpg",
+  },
+  {
+    label: "Mansions",
+    imgSrc:
+      "https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg",
+  },
+  {
+    label: "Tropical",
     imgSrc:
       "https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg",
   },
+  {
+    label: "Tiny Homes",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3271df99-f071-4ecf-9128-eb2d2b1f50f0.jpg",
+  },
+  {
+    label: "OMG",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
+  },
+  {
+    label: "Trending",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
+  },
+  {
+    label: "Earth Homes",
+    imgSrc:
+      "https://a0.muscache.com/pictures/d7445031-62c4-46d0-91c3-4f29f9790f7a.jpg",
+  },
+  {
+    label: "Castle",
+    imgSrc:
+      "https://a0.muscache.com/pictures/1b6a8b70-a3b6-48b5-88e1-2243d9172c06.jpg",
+  },
+  {
+    label: "Bed and BreakFast",
+    imgSrc:
+      "https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg",
+  },
+
 ];
