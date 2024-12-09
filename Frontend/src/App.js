4c5d90b0036e5d4b8dd89318cad607fe40579e65
Saif3256 <167188461+Saif3256@users.noreply.github.com>
2024-12-10 00:04:55 +0500

    Integration with backend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,30 @@
-import { useState } from "react";
 
-import { list } from "./assets/Cards-Description.js"; // Keep only one list imported
-import Multiple_Cards from "./components/Cards/card.js";
+
 import NavLogo from "./components/NavBar_Logos/NavLogo.js";
 import Header from "./components/Header";
 import Footer from "./components/Footer/Footer.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  const [selectedFilter, setSelectedFilter] = useState(0);
+import Home from "./Pages/Homepage.jsx";
+import BookingPage from "./Pages/BookingPage";
 
-  // Since we have a single list now, we can use it directly
+const App = () => {
   return (
-    <div className="App">
-      <Header/>
-      <NavLogo
-        selectedFilter={selectedFilter}
-        setSelectedFilter={setSelectedFilter}
-      />
-      <Multiple_Cards list={list} /> {/* Use the single list here */}
-      <Footer/>
+    <div>
+      <Router>
+        
+        <Header />
+        <NavLogo />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+          <Route path="/book/:id" element={<BookingPage />} />
+        </Routes>
+   
+        <Footer />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
--- Frontend/src/Pages/Homepage.jsx
+++ Frontend/src/Pages/Homepage.jsx
@@ -1,21 +1,9 @@
-
-
-import Footer from "../components/Footer/Footer.js";
-
 import { list } from "../assets/Cards-Description.js"; // Keep only one list imported
 import Multiple_Cards from "../components/Cards/card.js";
-import SearchBar from "../Components/SearchBar";
-
-
 const Home = () => {
   return (
     <div>
-     
-      <SearchBar />
-      
-      
       <Multiple_Cards list={list} />
-      <Footer/>
     </div>
   );
 };
